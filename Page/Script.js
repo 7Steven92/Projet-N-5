94e6e7502ac12bbb87fa356e842178f203a77009
Matheoin <120017099+Matheoin@users.noreply.github.com>
2025-03-18 15:12:23 +0100

    Update Script.js

--- Page/Script.js
+++ Page/Script.js
@@ -1,17 +1,18 @@
 // Modifier directement le contenu de la page web.
-var citationOuBlague, description;
+function bouton() {
+    var citationOuBlague, description;
 
-citationOuBlague = "citation ou blague"; //Citation ou blague générer
-document.getElementById("citation/blague").innerHTML = citationOuBlague;
+    citationOuBlague = "citation ou blague"; //Citation ou blague générer;
+    document.getElementById("citation/blague").innerHTML = citationOuBlague;
 
-description = "Déscription de la citation ou blague"; //Déscription de la citation ou blague
-document.getElementById("description").innerHTML = description;
-
+    description = "Déscription de la citation ou blague"; //Déscription de la citation ou blague
+    document.getElementById("description").innerHTML = description; 
+}
 
 // Ouverture/Fermeture de la déscription.
 const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("ouvrir");
-const closeButton = document.querySelector("fermer");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
 
 showButton.addEventListener("click", () => {
   dialog.showModal();
