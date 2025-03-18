1e6e8b98207e22cbae48345ac6f4b1b362fec232
Matheoin <120017099+Matheoin@users.noreply.github.com>
2025-03-18 16:51:36 +0100

    Update Script.js

--- Page/Script.js
+++ Page/Script.js
@@ -1,3 +1,38 @@
+// Génération des blagues.
+fetch('https://www.blagues-api.fr/api/random', {
+    headers: {
+      'Authorization': ` Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjg3MzQ5NzY5NTk2Njk4NjQzIiwibGltaXQiOjEwMCwia2V5IjoiWHZIZTVRbEJuRHYzcDBoeVlKQU9ldUU4TmZ0Y2lBSmJONElRR1lXbmtWNEhtb1A5dzQiLCJjcmVhdGVkX2F0IjoiMjAyNS0wMy0xOFQxMjo1MToyNyswMDowMCIsImlhdCI6MTc0MjMwMjI4N30.HG3q3Xc5eofHGrz4Jv3DPh6rxA3-3RyakicCFQt4o30 `
+    }
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+
+    if (data.joke && data.answer){
+        document.getElementById('type').textContent = data.type;
+        document.getElementById('joke').textContent = data.joke;
+        document.getElementById('answer').textContent = data.answer;
+    }
+    else {
+        console.error('Données incorrectes reçues', data);
+    }
+})
+.catch (error => console.error("Erreur lors de la récupération de la blague", error));
+  
+
+  
+
+Questions = [
+    "Comment appelle-t-on un choux qui vit sous l'eau ?",
+    "Comment appelle-t-on un hamster qui vit dans la lune ?",
+
+]
+Reponses = [
+    "Un choux marin",
+    "un hamsteroid",
+
+]
+
 // Modifier directement le contenu de la page web.
 function bouton() {
     var citationOuBlague, description;
